98a0be2893629238a48caf5209ca191fe0ea9411
jomaenfe <enriquezmanueljose@hotmail.com>
2018-10-15 01:11:33 +0200

    Cambiando setup.py

--- planificador/setup.py
+++ planificador/setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='planificador',
     packages=['planificador'],
-    version='1.2.1',
+    version='1.2.2',
     description='Hito 2 del proyecto, pasando test con travis',
     url='https://github.com/jomaenfe/Planificador_diario-IV1819/tree/master/planificador',    
 )
