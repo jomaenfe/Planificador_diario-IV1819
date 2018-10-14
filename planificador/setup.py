c162b91f668ef58dafb3822e30501c0ad3be31eb
jomaenfe <enriquezmanueljose@hotmail.com>
2018-10-14 20:08:34 +0200

    Subiendo archivo de metainformacion del proyecto

---
+++ planificador/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='planificador',
+    packages=['planificador'],
+    version='1.2',
+    description='Hito 2 del proyecto, pasando test con travis',
+    url='https://github.com/jomaenfe/Planificador_diario-IV1819/tree/master/planificador',    
+)
