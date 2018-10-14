089b0c473207df0581ce1db4ca77cf1e64345551
jomaenfe <enriquezmanueljose@hotmail.com>
2018-10-14 19:51:36 +0200

    Correccion de archivos

--- planificador/setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup
-
-setup(
-    name='planificador',
-    packages=['planificador'],
-    version='1.2',
-    description='Hito 2 del proyecto, pasando test con travis',
-    url='https://github.com/jomaenfe/Planificador_diario-IV1819/tree/master/planificador',    
-)
