c2c2943af5f095b09873719b7f492152f5d67cd7
jomaenfe <enriquezmanueljose@hotmail.com>
2018-10-15 02:10:53 +0200

    Borrando archivos innecesarios

--- planificador/setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup
-
-setup(
-    name='planificador',
-    packages=['planificador'],
-    version='1.2.3',
-    description='Hito 2 del proyecto, pasando test con travis',
-    url='https://github.com/jomaenfe/Planificador_diario-IV1819/planificador',    
-)
