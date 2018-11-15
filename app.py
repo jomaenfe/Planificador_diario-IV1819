af6a6f9b48795a9e3fc0cd7ec93d4e50daa9d122
jomaenfe <enriquezmanueljose@hotmail.com>
2018-11-15 18:28:50 +0100

    Corrigiendo archivo app.py con los nuevos json closes #18

--- app.py
+++ app.py
@@ -3,7 +3,12 @@ import json
 from src.planificador_diario import Planificador_diario
 
 estadoSimple = {
-    "status": "OK"
+    "status": "OK",
+    "ejemplo": {
+            "ruta":"/status",
+            "valor":"{ 'dia': '09', 'mes':'11', 'anio':'2018', 'plan': 'celebrar cumpleanios', 'hora_inicio': '21', 'minuto_inicio':'00', 'hora_fin':'23', 'minuto_fin':'30' }"
+    } 
+
 }
 
 @hug.get('/')
@@ -19,7 +24,6 @@ def status():
     if value==True:
         with open('status.json') as j:
             respuesta = json.load(j)
-            
 
 
     return respuesta
