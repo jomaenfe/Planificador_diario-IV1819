b9bd865104cc7003223f7037564ef8e847ec9766
jomaenfe <enriquezmanueljose@hotmail.com>
2018-12-27 18:23:40 +0100

    Mejorando fabfile closes #38

--- despliegue/fabfile.py
+++ despliegue/fabfile.py
@@ -7,6 +7,14 @@
 # debemos instalar pip3 y como módulo instalar fabric3
 from fabric.api import *
 
+env.hosts = [
+     'planificador-diario-iv1819.westeurope.cloudapp.azure.com',
+]
+
+env.user = 'vagrant'
+
+env.always_use_pty = False
+
 # En el caso de tener que actualizar el código, el primer paso sería borrar lo que tenemos, en segundo paso volver a clonar la app
 # y como tercer paso instalar los requerimientos por si han cambiado.
 def Actualizar():
@@ -20,8 +28,8 @@ def Actualizar():
 # Para iniciar el servicio web usamos esta función.
 def Iniciar():
 
-    
-     run('cd Planificador_diario-IV1819/ && sudo gunicorn app:__hug_wsgi__ -b 0.0.0.0:80')
+    run('cd Planificador_diario-IV1819/ && sudo sh iniciar_gunicorn.sh')
+     #run('cd Planificador_diario-IV1819/ && sudo gunicorn app:__hug_wsgi__ -b 0.0.0.0:80 &')
 
 # Para detener el servicio web usamos esta función.
 def Detener():
